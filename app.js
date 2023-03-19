2deef0bae81d4f3a2abd70af9a8c2cdb9fb83a84
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-03-19 07:06:56 +0600

    buttons, and display get using id on JS app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+
+
+start.addEventListener("click", () => {
+    window.alert("clicked")
+});
+
+stop.addEventListener("click", () => {
+    window.alert("clicked")
+});
+
+reset.addEventListener("click", () => {
+    window.alert("clicked")
+});
